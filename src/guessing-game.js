2d0047a0d85873fc59a8ca8901ba82e25a21d762
komalapa <komalapa@gmail.com>
2021-08-04 22:54:06 +0400

    feat: solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,28 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        console.log(this.min, this.max)
     }
 
     guess() {
-
+        if (this.max-this.min>1){ this.solution = Math.round((this.max+this.min)/2)
+        console.log("it's ",this.solution)
+        return this.solution
+        }
+        if (this.max === this.solution) return this.min
+        return this.max
     }
 
     lower() {
-
+        this.max = this.solution
+        //console.log("lower",this.min,this.max,)
     }
 
     greater() {
-
+        this.min = this.solution
+        //console.log("greater",this.min,this.max,)
     }
 }
 
